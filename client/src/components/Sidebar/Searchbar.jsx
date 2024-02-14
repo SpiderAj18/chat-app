20c37c458220397e154640acda64837a14ce0743
Ajay Gupta <ag18102001@gmail.com>
2024-02-14 23:34:39 +0530

    build(deps): update package-lock.json, package.json and Searchbar.jsx

--- client/src/components/Sidebar/Searchbar.jsx
+++ client/src/components/Sidebar/Searchbar.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const Searchbar = () => {
   return (
-    <div className="join">
-  <div>
-    <div>
-      <input className="input rounded-full input-bordered join-item" placeholder="Search"/>
-    </div>
-  </div>
- 
-  <div className="indicator ">
-    {/* <span className="indicator-item badge badge-secondary">new</span>  */}
-    <button className="btn join-item rounded-full">Search</button>
-  </div>
-</div>
+    <form action="" className='flex items-center gap-2'>
+        <input type="text" placeholder='search' className='input input-bordered rounded-full' />
+        <button className='btn btn-circle bg-sky-600 text-white'><SearchOutlinedIcon/></button>
+    </form>
   )
 }
 
